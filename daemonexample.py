b2ddbf0cf420374897732b270bf34a628aca3254
martinrusev <martinrusev@zoho.com>
2011-08-28 11:28:43 +0100

    Exception logging added

--- daemonexample.py
+++ daemonexample.py
@@ -2,16 +2,28 @@
 from daemon import Daemon
 import sys
 import time
+import logging
 
 PIDFILE = '/var/run/yourdaemon.pid'
+LOGFILE = '/var/log/yourdaemon.log'
+
+# Configure logging
+logging.basicConfig(filename=LOGFILE,level=logging.DEBUG)
 
 class YourDaemon(Daemon):
-	
+
 	def run(self):
 		# Define your tasks here
 		# Anything written in python is permitted
 		# For example you can clean up your server logs every hour
-			
+		
+		
+		# Logging errors and exceptions
+		try:
+			pass
+		except Exception, e:
+			logging.exception('Human friendly error message, the exception will be captured and added to the log file automaticaly')
+
 		while True:
 			# The daemon will repeat your tasks according to this variable
 			# it's in second so 60 is 1 minute, 3600 is 1 hour, etc. 
@@ -20,7 +32,7 @@ class YourDaemon(Daemon):
 if __name__ == "__main__":
 
 	daemon = YourDaemon(PIDFILE)
-	
+
 	if len(sys.argv) == 2:
 
 		if 'start' == sys.argv[1]:
@@ -28,15 +40,15 @@ if __name__ == "__main__":
 				daemon.start()
 			except:
 				pass
-		
+
 		elif 'stop' == sys.argv[1]:
 			print "Stopping ..."
 			daemon.stop()
-		
+
 		elif 'restart' == sys.argv[1]:
 			print "Restaring ..."
 			daemon.restart()
-		
+
 		elif 'status' == sys.argv[1]:
 			try:
 				pf = file(PIDFILE,'r')
@@ -51,7 +63,7 @@ if __name__ == "__main__":
 				print 'YourDaemon is running as pid %s' % pid
 			else:
 				print 'YourDaemon is not running.'
-		
+
 		else:
 			print "Unknown command"
 			sys.exit(2)
